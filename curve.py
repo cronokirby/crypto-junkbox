b07e92408c8f805a043f16be6eb0e40f561d4511
Lúcás Meier <cronokirby@gmail.com>
2021-04-23 08:56:28 +0200

    Add displaying curves

--- curve.py
+++ curve.py
@@ -62,3 +62,9 @@ class Curve:
         self.B = B
         self.A = A
         self.P = P
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return f"Curve(B={self.B:_X}, A={self.A:_X}, P={self.P})"
