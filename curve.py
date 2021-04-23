e33700b176562d85dd59ecc52d01259917eed5bc
Lúcás Meier <cronokirby@gmail.com>
2021-04-23 09:57:27 +0200

    Add stuff

--- curve.py
+++ curve.py
@@ -48,6 +48,17 @@ class Modulus:
         Calculate x^(-1) % P, assuming x is already reduced
         """
         return self.exp(x, self.P - 2)
+    
+    def negate(self, x):
+        """
+        Calculate -x % P
+        """
+        if x == 0:
+            return x
+        return self.P - x
+
+    def sub(self, a, b):
+        return self.add(a, self.negate(b))
 
     def __str__(self):
         return f"Modulus({self.P:_X})"
@@ -62,14 +73,26 @@ class Point:
     Represents a Point on the curve
     """
 
-    infinity: bool
+    is_infinity: bool
     x: int
     y: int
+
+    @staticmethod
+    def infinity():
+        return Point(True, 0, 0)
 
     def to_projective(self):
         if self.infinity:
             return Projective(0, 1, 0)
-        return Projective(x, y, 1)
+        return Projective(self.x, self.y, 1)
+    
+    def __repr__(self):
+        return self.__str__()
+    
+    def __str__(self):
+        if self.is_infinity:
+            return "Point(∞)"
+        return f"Point(x={self.x:_X} y={self.y:_X})"
 
 
 @dataclass
@@ -80,7 +103,7 @@ class Projective:
 
     def project(self, P: Modulus):
         if self.z == 0:
-            return Point(True, 0, 0)
+            return Point.infinity()
         zinv = P.invert(self.z)
         return Point(False, self.x * zinv, self.y * zinv)
 
@@ -106,3 +129,22 @@ class Curve:
         x3 = self.P.mul(x, x2)
         out = self.P.add(x3, x)
         return self.P.add(out, self.P.mul(self.A, x2))
+
+    def add(self, p: Point, q: Point):
+        if p.is_infinity:
+            return q
+        if q.is_infinity:
+            return p
+        l = 0
+        if p.x == q.x:
+            if p.y != q.y:
+                return Point.infinity()
+            l = self.P.reduce(3 * p.x * p.x + 2 * self.A * p.x + 1)
+            l = self.P.mul(l, self.P.invert(self.P.reduce(2 * self.B * p.y)))
+        l = self.P.sub(q.y, p.y)
+        l = self.P.mul(l, self.P.invert(self.P.sub(q.x, q.y)))
+        l2 = self.P.mul(l, l)
+        out_x = self.P.reduce(self.B * l2 - self.A - p.x - q.x)
+        out_y = self.P.reduce(l * (p.x - out_x) - p.y)
+        return Point(False, out_x, out_y)
+
